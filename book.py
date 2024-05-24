86ebce30c908f1fab74469da8ea056400872ef3f
Alton <smithiialton@gmail.com>
2024-05-24 10:54:00 -0700

    completed book details method

--- book.py
+++ book.py
@@ -11,10 +11,34 @@ class Book:
     def book_details(self):
         while True:
             try:
-                to_display = input("enter an option for information\n1. Genre\n2. Author\n3. Title\n4. Price\n5. In Stock\n6. quit")
+                to_display = int(input("enter an option for information\n1. Genre\n2. Author\n3. Title\n4. Price\n5. In Stock\n6. Quit\n"))
 
-            except:
-                pass
+                if to_display == 6:
+                    print("Goodbye")
+                    break
+
+                elif to_display not in range(1,6):
+                    print("please choose one of the options")
+                    continue
+                
+                elif to_display == 1:
+                    print(f"Genre: {self.genre}")
+                
+                elif to_display == 2:
+                    print(f"Author: {self.author}")
+                
+                elif to_display == 3:
+                    print(f'Title: "{self.title}"')
+                
+                elif to_display == 4:
+                    print(f"Price: {self.price}")
+                
+                elif to_display == 5:
+                    print(f"InStock: {self.in_stock}")
+
+            except ValueError:
+                print("Response must be an integer between 1 and 6")
+                continue
 
 
 
@@ -29,3 +53,4 @@ print(book1.in_stock)
 
 
 book1.book_details()
+
