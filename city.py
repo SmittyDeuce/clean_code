94aeb178b609165a57f10e65b9ffa3dd1a7879f5
Alton <smithiialton@gmail.com>
2024-05-27 06:10:27 -0700

    completed parser

--- city.py
+++ city.py
@@ -1,16 +1,17 @@
 class City:
-    def __init__(self, name, temperature, weather, humidity):
+    def __init__(self, name, temperature, condition, humidity):
         
         self.name = name
         self.temperature = int(temperature)
-        self.weather = weather
+        self.condition= condition
         self.humidity = int(humidity)
 
-    def fetch_weather_data(self):
-
-        return f"City: {self.name}\nTemp: {self.temperature}\nWeather: {self.weather}\nHumidity: {self.humidity}"
-    
+    def city_details(self):
+        return (f"City: {self.name}\n"
+                f"Temperature: {self.temperature}\n"
+                f"Condition: {self.condition}\n"
+                f"Humidity: {self.humidity}%")
 
 nyc = City("New York City", 70, "Sunny",50)
 
-print(nyc.fetch_weather_data())
+print(nyc.city_details())
--- fetcher.py
+++ fetcher.py
@@ -1,10 +1,14 @@
 from city import City
 
 class WeatherFetcher:
-    def fetch_data(self,city_name):
-
+    def fetch_weather_data(self, city_name):
         print(f"Fetching weather data for {city_name}...")
-        # Simulated data based on city
-        weather_data = {}
-            
-        return weather_data.get(city_name, {})
+        weather_data = {
+            "New York": {"temperature": 70, "condition": "Sunny", "humidity": 50},
+            "London": {"temperature": 60, "condition": "Cloudy", "humidity": 65},
+            "Tokyo": {"temperature": 75, "condition": "Rainy", "humidity": 70}
+        }
+        data = weather_data.get(city_name)
+        if data:
+            return City(city_name, data["temperature"], data["condition"], data["humidity"])
+        return None
